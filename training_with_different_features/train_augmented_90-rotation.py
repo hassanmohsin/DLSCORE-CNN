453aa27a95556221bf4c1bc45485652fec44a887
Mahmudulla Hassan <mhassan@miners.utep.edu>
2018-09-12 00:41:19 -0600

    Convert notebooks and elements.py in scripts

---
+++ training_with_different_features/train_augmented_90-rotation.py
@@ -0,0 +1,246 @@
+
+# coding: utf-8
+
+# ## Training
+
+# In[2]:
+
+
+from keras.models import Sequential,Model
+from keras.layers import Dropout, Flatten, Dense, Input, Add, merge, concatenate
+from keras.layers.convolutional import Conv3D
+from keras.layers.pooling import MaxPooling3D, GlobalAveragePooling3D, AveragePooling3D
+from keras import metrics
+from keras import optimizers
+from keras.utils import plot_model
+from keras import backend as K
+from keras.utils.training_utils import multi_gpu_model
+from keras.utils.data_utils import Sequence
+from keras.callbacks import ModelCheckpoint
+from keras.initializers import he_uniform
+from keras.initializers import glorot_uniform
+
+import numpy as np
+import sys
+import h5py
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+sys.path.append("models/")
+sys.path.append("scripts/")
+
+
+# In[3]:
+
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0, 1, 2, 3"
+
+
+# In[4]:
+
+
+from my_classes import DataGenerator, AugmentedDataGenerator
+
+
+# In[5]:
+
+
+# Load the data
+h5f = h5py.File('data/data_nearest_neighbor.h5', 'r')
+train_x, train_y = h5f['train_x'][:], h5f['train_y'][:]
+valid_x, valid_y = h5f['valid_x'][:], h5f['valid_y'][:]
+test_x, test_y = h5f['test_x'][:], h5f['test_y'][:]
+h5f.close()
+
+print(train_x.shape, valid_x.shape, test_x.shape)
+
+
+# In[6]:
+
+
+from models import Squeeze_model
+
+
+# In[7]:
+
+
+# from IPython.display import SVG
+# from keras.utils.vis_utils import plot_model, model_to_dot
+# model_input = Input(shape=(24, 24, 24, 16))
+# squeeze_model = Model(inputs=model_input, outputs=Squeeze_model(model_input))
+# #plot_model(squeeze_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+# SVG(model_to_dot(squeeze_model, show_layer_names=True, show_shapes=True).create(prog='dot', format='svg'))
+# # plot_model(get_model4((24, 24, 24, 16)))
+
+
+# In[8]:
+
+
+# Training parameters
+nb_gpus = 4
+nb_batch = nb_gpus*3
+nb_epochs =200
+l_rate = 1e-4
+
+
+# In[9]:
+
+
+# checkpoint
+outputFolder = './weights'
+# if not os.path.exists(outputFolder):
+#     os.makedirs(outputFolder)
+
+filepath=outputFolder+"/weights-rotated_90.h5"
+
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+callbacks_list = [ModelCheckpoint(filepath, 
+                                  monitor='val_loss',
+                                  verbose=1,
+                                  save_best_only=True,
+                                  save_weights_only=True,
+                                  mode='auto', period=1)]
+
+
+# In[10]:
+
+
+model_input = Input(shape=(24, 24, 24, 16))
+squeeze_model = Model(inputs=model_input, outputs=Squeeze_model(model_input))
+model = multi_gpu_model(squeeze_model, gpus=nb_gpus)
+
+model.compile(optimizer=optimizers.adam(lr=l_rate),# beta_1=0.99, beta_2=0.999),
+              loss='mean_absolute_error')
+
+
+# In[11]:
+
+
+aug_data_gen = AugmentedDataGenerator(x=train_x, y=train_y, batch_size=nb_batch)
+aug_val_gen = AugmentedDataGenerator(x=valid_x, y=valid_y, batch_size=nb_batch)
+
+
+# In[12]:
+
+
+for x, y in aug_data_gen:
+    print(x.shape, y.shape)
+    break
+
+
+# In[13]:
+
+
+history = model.fit_generator(generator=aug_data_gen,validation_data=aug_val_gen,
+                              use_multiprocessing=False, 
+                              epochs=nb_epochs, 
+                              max_queue_size=5, 
+                              workers=56, 
+                              verbose=1, callbacks = callbacks_list)
+
+
+# In[14]:
+
+
+# Save the history
+import pickle
+
+with open(os.path.join(outputFolder, "history_rotated_90.pickle"), 'wb') as f:
+    pickle.dump(history.history, f)
+
+
+# In[15]:
+
+
+# First 100 epochs
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.show()
+
+
+# In[33]:
+
+
+# Save the weights
+# model.save_weights('weights/weights_rotated_90.h5')
+
+
+# In[16]:
+
+
+from sklearn.metrics import r2_score
+
+
+# In[17]:
+
+
+model.load_weights("weights/weights-rotated_90.h5")
+
+
+# In[18]:
+
+
+sample_size = 200
+rotation_count = 24
+
+train_aug_data_gen = AugmentedDataGenerator(x=train_x,
+                                            y=train_y,
+                                            batch_size=sample_size)
+# Two chunks to avoid memory issues
+test_aug_data_gen= AugmentedDataGenerator(x=test_x[:sample_size],
+                                            y=test_y[:sample_size],
+                                            batch_size=sample_size)
+
+# Train r2
+
+for x, y in train_aug_data_gen:
+    # Without average
+    train_r2 = r2_score(y_true=y, y_pred=model.predict(x))
+    print("Train r2 (without average): ", train_r2)
+    
+    # With average
+    y_pred = model.predict(x)[:, 0]
+    
+    sample_y = np.zeros(sample_size)
+    sample_ypred = np.zeros(sample_size)
+    for i in range(sample_size):
+        start = i*rotation_count
+        end = i*rotation_count + rotation_count
+        mean_ypred = np.mean(y_pred[start:end])
+        mean_y = np.mean(y[start:end])
+        sample_ypred[i] = mean_ypred
+        sample_y[i] = mean_y
+    
+    train_r2 = r2_score(y_true=sample_y, y_pred=sample_ypred)
+    print("Train r2 (with average): ", train_r2)
+    break
+    
+#sample_size = test_x.shape[0]
+    
+# Test r2
+
+for x, y in test_aug_data_gen:
+    # Without average
+    test_r2 = r2_score(y_true=y, y_pred=model.predict(x))
+    print("Test r2 (without average): ", test_r2)
+    
+    # With average
+    y_pred = model.predict(x)[:, 0]
+    
+    sample_y = np.zeros(sample_size)
+    sample_ypred = np.zeros(sample_size)
+    for i in range(sample_size):
+        start = i*rotation_count
+        end = i*rotation_count + rotation_count
+        mean_ypred = np.mean(y_pred[start:end])
+        mean_y = np.mean(y[start:end])
+        sample_ypred[i] = mean_ypred
+        sample_y[i] = mean_y
+    
+    test_r2 = r2_score(y_true=sample_y, y_pred=sample_ypred)
+    print("Test r2 (with average): ", test_r2)
+    break
+
---
+++ training_with_different_features/train_distributed_original.py
@@ -0,0 +1,171 @@
+
+# coding: utf-8
+
+# ## Training
+
+# In[1]:
+
+
+from keras.models import Sequential,Model
+from keras.layers import Dropout, Flatten, Dense, Input, Add, merge, concatenate
+from keras.layers.convolutional import Conv3D
+from keras.layers.pooling import MaxPooling3D, GlobalAveragePooling3D, AveragePooling3D
+from keras import metrics
+from keras import optimizers
+from keras.utils import plot_model
+from keras import backend as K
+from keras.utils.training_utils import multi_gpu_model
+from keras.utils.data_utils import Sequence
+from keras.callbacks import ModelCheckpoint
+from keras.initializers import he_uniform
+from keras.initializers import glorot_uniform
+
+import numpy as np
+import sys
+import h5py
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+sys.path.append("models/")
+sys.path.append("scripts/")
+
+
+# In[2]:
+
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0, 1, 2, 3"
+
+
+# In[3]:
+
+
+from my_classes import DataGenerator, AugmentedDataGenerator
+
+
+# In[5]:
+
+
+# Load the data
+h5f = h5py.File('data/data_distributed2.h5', 'r')
+train_x, train_y = h5f['train_x'][:], h5f['train_y'][:]
+valid_x, valid_y = h5f['valid_x'][:], h5f['valid_y'][:]
+test_x, test_y = h5f['test_x'][:], h5f['test_y'][:]
+h5f.close()
+
+print(train_x.shape, valid_x.shape, test_x.shape)
+
+
+# In[3]:
+
+
+from models import Squeeze_model
+
+
+# In[39]:
+
+
+# Training parameters
+nb_gpus = 4
+nb_batch = nb_gpus*16
+nb_epochs = 100
+l_rate = 1e-5
+
+
+# In[40]:
+
+
+data_gen = DataGenerator(x=train_x, y=train_y, batch_size=nb_batch)
+val_gen = DataGenerator(x=valid_x, y=valid_y, batch_size=nb_batch)
+
+
+# In[41]:
+
+
+# checkpoint
+outputFolder = './weights'
+# if not os.path.exists(outputFolder):
+#     os.makedirs(outputFolder)
+
+filepath=outputFolder+"/weights-distributed_original.h5"
+
+callbacks_list = [ModelCheckpoint(filepath, 
+                                  monitor='val_loss',
+                                  verbose=1,
+                                  save_best_only=True,
+                                  save_weights_only=True,
+                                  mode='auto', period=1)]
+
+
+# In[42]:
+
+
+# Build the model and train
+model_input = Input(shape=(24, 24, 24, 16))
+squeeze_model = Model(inputs=model_input, outputs=Squeeze_model(model_input))
+model = multi_gpu_model(squeeze_model, gpus=nb_gpus)
+model.compile(optimizer=optimizers.adam(lr=l_rate),# decay=decay_rate, beta_1=0.99, beta_2=0.999),
+              loss='mean_absolute_error')
+              #target_tensors=[staging_area_callback.target_tensor],
+              #fetches=staging_area_callback.extra_ops)
+
+
+# In[43]:
+
+
+history = model.fit_generator(generator=data_gen, validation_data=val_gen,
+                              use_multiprocessing=False, 
+                              epochs=nb_epochs, 
+                              max_queue_size=10, 
+                              workers=56, 
+                              verbose=1, callbacks = callbacks_list)
+
+
+# In[44]:
+
+
+# Save the history
+import pickle
+
+with open(os.path.join(outputFolder, "history_distributed_original.pickle"), 'wb') as f:
+    pickle.dump(history.history, f)
+
+
+# In[45]:
+
+
+# First 100 epochs
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.show()
+
+
+# In[46]:
+
+
+from sklearn.metrics import r2_score
+
+
+# In[47]:
+
+
+model.load_weights(filepath)
+
+
+# In[48]:
+
+
+#train_r2 = r2_score(y_true=train_y, y_pred=model.predict(train_x))
+train_r2 = r2_score(y_true=train_y[:2000], y_pred=model.predict(train_x[:2000]))
+print("Train r2: ", train_r2)
+
+
+# In[49]:
+
+
+#train_r2 = r2_score(y_true=train_y, y_pred=model.predict(train_x))
+test_r2 = r2_score(y_true=test_y, y_pred=model.predict(test_x))
+print("Test r2: ", test_r2)
+
